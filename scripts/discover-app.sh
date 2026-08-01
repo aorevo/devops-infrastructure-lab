86352deddfc3e11bd0a354c098d5f51780c1e165
sj <y>
2026-08-01 15:32:25 +0000

    minor changes

--- scripts/discover-app.sh
+++ scripts/discover-app.sh
@@ -8,8 +8,6 @@ found_unhealthy=0
 
 while read -r state recv_q send_q local_address peer_address process_info; do
 
-    [[ "$process_info" != *python3* ]] && continue
-
     pid_part="${process_info#*pid=}"
     pid_backend="${pid_part%%,*}"
     port_backend="${local_address##*:}"
@@ -22,18 +20,18 @@ while read -r state recv_q send_q local_address peer_address process_info; do
 
     ((found += 1))
 
-    echo -e "Найдено приложение\nPID: $pid_backend\nPORT: $port_backend\nCOMMAND: $link_to_backend\n"
+    echo -e "====Найдено приложение====\nPID: $pid_backend\nPORT: $port_backend\nCOMMAND: $link_to_backend\n"
 
     if status_code=$(curl -s --connect-timeout 2 -o /dev/null -w "%{http_code}" http://127.0.0.1:$port_backend/api/health); then
         if [[ $status_code -eq 200 ]]; then
-            echo -e "Приложение запущено успешно\nHTTP: $status_code\nHEALTHY"
+            echo -e "Приложение запущено успешно\nHTTP: $status_code\nHEALTHY\n=====\n"
             ((found_healthy += 1))
         else
-            echo -e "Внимание\nHTTP: $status_code\nUNHEALTHY"
+            echo -e "Внимание\nHTTP: $status_code\nUNHEALTHY\n=====\n"
             ((found_unhealthy += 1))
         fi
     else
-        echo "Не удалось подключиться"
+        echo "Не удалось подключиться\n=====\n"
         ((found_unhealthy += 1))
     fi
 
@@ -48,6 +46,6 @@ if [[ "$found_unhealthy" -ne 0 ]]; then
     echo -e "Найдено $found_unhealthy UNHEALTHY экземпляров\n"
     exit 1
 else
-    echo -e "\n======\nВсе найденные экземпляры HEALTHY\nКоличество: $found_healthy"
+    echo -e "\n===Итог===\nВсе найденные экземпляры HEALTHY\nКоличество: $found_healthy"
     exit 0
 fi
