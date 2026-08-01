8f1c0cbf882a347a5928534a35ec5b5ab78726f0
sj <y>
2026-08-01 14:47:46 +0000

    added discovery-app script

---
+++ scripts/discover-app.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -uo pipefail
+
+found=0;
+
+while read -r state recv_q send_q local_address peer_address process_info; do
+
+    [[ "$process_info" != *python3* ]] && continue
+
+    pid_part="${process_info#*pid=}"
+    pid_backend="${pid_part%%,*}"
+    port_backend="${local_address##*:}"
+
+    [[ -r "/proc/$pid_backend/cmdline" ]] || continue
+
+    link_to_backend=$(tr '\0' ' ' < /proc/"$pid_backend"/cmdline)
+
+    [[ "$link_to_backend" != *"app/main.py"* ]] && continue
+
+    found=1
+
+    echo -e "Найдено приложение\nPID: $pid_backend\nPORT: $port_backend\nCOMMAND: $link_to_backend\n"
+
+    if status_code=$(curl -s --connect-timeout 2 -o /dev/null -w "%{http_code}" http://127.0.0.1:$port_backend/api/health); then
+        if [[ $status_code -eq 200 ]]; then
+            echo -e "Приложение запущено успешно\nHTTP: $status_code\nHEALTHY"
+        else
+            echo -e "Внимание\nHTTP: $status_code\nUNHEALTHY"
+        fi
+    else
+        echo "Не удалось подключиться"
+    fi
+
+done < <(ss -H -tlpn) 
+
+if [[ "$found" -eq 0 ]]; then
+    echo "Экземпляры приложения не найдены"
+fi
